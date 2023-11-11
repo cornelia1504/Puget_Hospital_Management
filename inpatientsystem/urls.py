748c4defc83f2288d3c3c6309a366439336949d5
YANG Lan <lan.yang.1@etu.univ-amu.fr>
2023-11-11 08:19:47 +0100

    write some homepage html files but not finished

--- inpatientsystem/urls.py
+++ inpatientsystem/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from inpatientsystemApp.views import homepage
+from inpatientsystemApp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('homepage/', homepage),
+    path('', views.homepage),
 ]
